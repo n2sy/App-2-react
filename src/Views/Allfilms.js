c5800f06b4a60ed4f5f62d264be48ea5dac17568
nidhal <nidhal.jelassi@fsegt.utm.tn>
2022-06-29 14:15:47 +0100

    add get

--- src/Views/Allfilms.js
+++ src/Views/Allfilms.js
@@ -2,21 +2,35 @@ import FilmList from '../components/FilmList';
 
 function AllFilms() {
     const listFilms = [
-        {
-            id: 'f1',
-            title: 'Heat',
-            year: 1995,
-            image: 'https://www.premiere.fr/sites/default/files/styles/scale_crop_1280x720/public/2019-08/heat.jpg',
-            description: "La bande de Neil McCauley à laquelle est venu se greffer Waingro, une nouvelle recrue, attaque un fourgon blindé pour s'emparer d'une somme importante en obligations."
-        },
-        {
-            id: 'f2',
-            title: 'Inception',
-            year: 2010,
-            image: 'https://cdn2.daily-movies.ch/wp-content/uploads/2014/11/daily-movies.ch_Inception.jpeg',
-            description: "Dom Cobb est un voleur expérimenté dans l'art périlleux de `l'extraction' : sa spécialité consiste à s'approprier les secrets les plus précieux d'un individu"
-        }
+        // {
+        //     id: 'f1',
+        //     title: 'Heat',
+        //     year: 1995,
+        //     image: 'https://www.premiere.fr/sites/default/files/styles/scale_crop_1280x720/public/2019-08/heat.jpg',
+        //     description: "La bande de Neil McCauley à laquelle est venu se greffer Waingro, une nouvelle recrue, attaque un fourgon blindé pour s'emparer d'une somme importante en obligations."
+        // },
+        // {
+        //     id: 'f2',
+        //     title: 'Inception',
+        //     year: 2010,
+        //     image: 'https://cdn2.daily-movies.ch/wp-content/uploads/2014/11/daily-movies.ch_Inception.jpeg',
+        //     description: "Dom Cobb est un voleur expérimenté dans l'art périlleux de `l'extraction' : sa spécialité consiste à s'approprier les secrets les plus précieux d'un individu"
+        // }
     ]
+
+    fetch('https://filmstore-409b9-default-rtdb.firebaseio.com/Films.json')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            for (const key in data) {
+                data[key].id = key;
+                listFilms.push(data[key]);
+            }
+            console.log(listFilms);
+        })
+
     return (
         <section>
             <h4>All Films</h4>
